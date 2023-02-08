4a5b307077594987b9c7a25fe9e1a16a737db167
Jun-yong-lee <wnsdyd1569@naver.com>
2023-02-09 00:01:11 +0900

    Change classes

--- dataloader/yolodata.py
+++ dataloader/yolodata.py
@@ -18,7 +18,7 @@ class Yolodata(Dataset):
     valid_dir = "C:\\study_git\\depth_estimation\\Depth-Estimation\\datasets\\eval"
     valid_txt = "eval.txt"
 
-    class_str = ['pingpong', 'x']
+    class_str = ['pingpong', 'ignore']
 
     num_class = None
     img_data = []
